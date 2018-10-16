2872b89befc38186d6b0a7687670cd22bb1504e8
McLamy <44198876+McLamy@users.noreply.github.com>
2018-10-16 14:16:07 +0200

    Add files via upload

---
+++ timeTracker.py
@@ -0,0 +1,7 @@
+#Name des Projekts (Neues Projekt erstellen)
+#Neuer Eintrag in eine Liste
+#per Klick auf die Liste wird die Zeit addiert
+#Timer mit Start, Stop und Pause
+#CSV Liste, die man exportieren und Speichern kann
+
+print ("das wird der timeTracker")
